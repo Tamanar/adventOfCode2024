e5a443331dbb450b33ee55fcd1ec4e7be37adfd6
Tamanar <adressespourjeux@hotmail.com>
2024-12-01 17:46:10 +0100

    day1 answer response 2

--- day1.py
+++ day1.py
@@ -8,7 +8,10 @@ for line in documentplit:
     list2.append(line.split(" ")[-1])
 list1.sort()
 list2.sort()
-distance = 0
-for id1,id2 in zip(list1,list2):
-    distance += abs(int(id1)-int(id2))
-print(distance)
+similiarities = 0
+for id1 in list1:
+    score = list2.count(id1) * int(id1)
+    if score > 0:
+        similiarities += score
+        print(id1)
+print(similiarities)
