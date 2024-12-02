69a8642054b27500383af33375c1489b75890394
pierre lobert <pierrel@kickmaker.net>
2024-12-02 09:49:37 +0100

    day 2 recursive working

--- day2.py
+++ day2.py
@@ -5,32 +5,49 @@ documentplit = all_document.splitlines()
 list1 = []
 
 
-def safe(listToTest: list) -> bool:
+def safe(listToTest: list, profondeur) -> bool:
+    if profondeur > 1:
+        return False
+    profondeur += 1
     unsafe = True
-    lastNumber = lineInt[0]
-    if lineInt[1] - lineInt[0] > 0:
+    lastNumber = listToTest[0]
+    if listToTest[1] - listToTest[0] > 0:
         IsIncreasing = True
     else:
         IsIncreasing = False
-    for number in lineInt[1:]:
+    index = 0
+    for number in listToTest[1:]:
 
         if lastNumber == number:
-            unsafe = False
+            unsafe = (
+                safe(listToTest[:index] + listToTest[index + 1 :], profondeur)
+                or safe(listToTest[: index - 1] + listToTest[index:], profondeur)
+                or safe(listToTest[: index + 1] + listToTest[index + 2 :], profondeur)
+            )
             break
         diff = abs(lastNumber - number)
         if not (1 <= diff <= 3):
-            unsafe = False
+            unsafe = (
+                safe(listToTest[:index] + listToTest[index + 1 :], profondeur)
+                or safe(listToTest[: index - 1] + listToTest[index:], profondeur)
+                or safe(listToTest[: index + 1] + listToTest[index + 2 :], profondeur)
+            )
             break
         if (number - lastNumber > 0) != IsIncreasing:
-            unsafe = False
+            unsafe = (
+                safe(listToTest[:index] + listToTest[index + 1 :], profondeur)
+                or safe(listToTest[: index - 1] + listToTest[index:], profondeur)
+                or safe(listToTest[: index + 1] + listToTest[index + 2 :], profondeur)
+            )
             break
         lastNumber = number
+        index += 1
     return unsafe
 
 
 unsafe = 0
 for line in documentplit:
     lineInt = [int(stringInList) for stringInList in line.split(" ")]
-    if not (safe(lineInt)):
+    if not (safe(lineInt, 0)):
         unsafe += 1
 print(len(documentplit) - unsafe)
