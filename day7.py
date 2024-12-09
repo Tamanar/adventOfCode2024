9422d9dadd91001d037d380ac72105c2137fd310
pierre lobert <pierrel@kickmaker.net>
2024-12-09 11:38:33 +0100

    flake8

--- day7.py
+++ day7.py
@@ -1,16 +1,20 @@
 import operator
-ops = { "+": operator.add, "*": operator.sub }
-def ternary (n):
+
+ops = {"+": operator.add, "*": operator.sub}
+
+
+def ternary(n):
     if n == 0:
-        return '0'
+        return "0"
     nums = []
     while n:
         n, r = divmod(n, 3)
         nums.append(str(r))
-    return ''.join(reversed(nums))
+    return "".join(reversed(nums))
+
 
 def main(ops):
-    
+
     with open("input.txt", "r") as f:
         all_document = f.read()
     documentsplit = all_document.splitlines()
@@ -19,11 +23,11 @@ def main(ops):
         resultValue = line.split(":")
         dico[int(resultValue[0])] = resultValue[1].split(" ")[1:]
     somme = 0
-    for key,values in dico.items():
+    for key, values in dico.items():
         listBin = []
         result = 0
-        nbrOperator = len(values)-1
-        for index in range (pow(3,nbrOperator)):
+        nbrOperator = len(values) - 1
+        for index in range(pow(3, nbrOperator)):
             listBin.append(((ternary(index))))
         maxSize = len(listBin[-1])
         for stringBinaryIndex in range(len(listBin)):
@@ -32,21 +36,20 @@ def main(ops):
                 stringBinary = "0" + stringBinary
             listBin[stringBinaryIndex] = stringBinary
 
-
-        for AlloperatorPossibity in (listBin):
+        for AlloperatorPossibity in listBin:
             result = int(values[0])
             for operator in range(len(AlloperatorPossibity)):
                 if AlloperatorPossibity[operator] == "0":
-                    result += int(values[operator+1])
+                    result += int(values[operator + 1])
                 elif AlloperatorPossibity[operator] == "1":
-                    result *= int(values[operator+1])
+                    result *= int(values[operator + 1])
                 else:
-                    result = int(str(result) + values[operator+1])
+                    result = int(str(result) + values[operator + 1])
             if result == key:
                 print(key)
-                somme+=key
+                somme += key
                 break
-            
+
     print(somme)
 
 
--- day8.py
+++ day8.py
@@ -6,37 +6,55 @@ def main():
     allListAntipode = []
     for coordY in range(len(documentsplit)):
         for coordX in range(len(documentsplit[coordY])):
-            char  = documentsplit[coordY][coordX]
-            if char!= ".":
-                allListAntipode += search_similar(coordY,coordX,char,documentsplit,len(documentsplit),len(documentsplit[0]))
+            char = documentsplit[coordY][coordX]
+            if char != ".":
+                allListAntipode += search_similar(
+                    coordY,
+                    coordX,
+                    char,
+                    documentsplit,
+                    len(documentsplit),
+                    len(documentsplit[0]),
+                )
     print(len(allListAntipode))
     print((allListAntipode))
-    cleanList = cleanAntipode(allListAntipode,len(documentsplit),len(documentsplit[0]))
+    cleanList = cleanAntipode(
+        allListAntipode, len(documentsplit), len(documentsplit[0])
+    )
     print((cleanList))
     print(len(cleanList))
-def search_similar(coordYIn,coordXIn,charIn,documentsplit,maxX,maxY) ->list:
+
+
+def search_similar(coordYIn, coordXIn, charIn, documentsplit, maxX, maxY) -> list:
     listAntipode = []
     for coordY in range(len(documentsplit)):
         for coordX in range(len(documentsplit[coordY])):
-            char  = documentsplit[coordY][coordX]
+            char = documentsplit[coordY][coordX]
             if char == charIn:
-                if not(coordY==coordYIn and coordX==coordXIn):
-                    antipodepos = [coordY+(coordY-coordYIn),coordX+(coordX-coordXIn)]
-                    while 0<=antipodepos[0]<maxY and 0<=antipodepos[1]<maxX:
+                if not (coordY == coordYIn and coordX == coordXIn):
+                    antipodepos = [
+                        coordY + (coordY - coordYIn),
+                        coordX + (coordX - coordXIn),
+                    ]
+                    while 0 <= antipodepos[0] < maxY and 0 <= antipodepos[1] < maxX:
                         listAntipode.append(antipodepos[:])
-                        antipodepos[0]+=(coordY-coordYIn)
-                        antipodepos[1]+=(coordX-coordXIn)
-    listAntipode.append([coordYIn,coordXIn])
+                        antipodepos[0] += coordY - coordYIn
+                        antipodepos[1] += coordX - coordXIn
+    listAntipode.append([coordYIn, coordXIn])
     return listAntipode
-def cleanAntipode (listAllAntipode:list,maxX,maxY):
+
+
+def cleanAntipode(listAllAntipode: list, maxX, maxY):
     cleanListOut = []
     for antipodePos in listAllAntipode:
-        if (0<=antipodePos[0]<maxY and 0<=antipodePos[1]<maxX):
+        if 0 <= antipodePos[0] < maxY and 0 <= antipodePos[1] < maxX:
             cleanListOut.append(antipodePos)
     cleanList = []
     for antipodePosIndex in range(len(cleanListOut)):
-        if not(cleanListOut[antipodePosIndex] in cleanListOut[antipodePosIndex+1:]) :
+        if not (cleanListOut[antipodePosIndex] in cleanListOut[antipodePosIndex + 1 :]):
             cleanList.append(cleanListOut[antipodePosIndex])
     return cleanList
+
+
 if __name__ == "__main__":
     main()
