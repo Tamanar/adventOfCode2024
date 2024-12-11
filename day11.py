56363395310d179a10cd66f70ddad9d1d6e2d871
pierre lobert <pierrel@kickmaker.net>
2024-12-11 14:42:21 +0100

    should work with day 11 step 2 but its long

--- day11.py
+++ day11.py
@@ -1,13 +1,25 @@
+import time
+
+SumLine = 0
+
+
 def main():
     with open("input.txt", "r") as f:
         all_document = f.read()
     documentsplit = all_document.split(" ")
     line = [int(x) for x in documentsplit]
-    blinkingRange = 25
-    for blinking in range(blinkingRange):
+    blinkingRange = 75
+    lenTotal = search(line, 0, blinkingRange)
+    print(f"Result final = {SumLine}")
+
+
+def search(line, blinkingStart, blinkingRange):
+    global SumLine
+    lenTotal = len(line)
+    for blinking in range(blinkingStart, blinkingRange):
         print(f"{blinking+1}/{blinkingRange}")
-        print(f"current len = {len(line)}")
         newLine = []
+        startTime = time.time()
         for element in line:
             if element == 0:
                 newLine.append(1)
@@ -18,9 +30,17 @@ def main():
                 newLine.append(elementTwo)
             else:
                 newLine.append(element * 2024)
-        line = newLine[:]
-    print(f"total length = {len(line)}")
+        if len(newLine) >= 100000:
+            line = newLine[: int(len(newLine) / 2)]
+            lenTotal = search(
+                newLine[int(len(newLine) / 2) :], blinking + 1, blinkingRange
+            )
+        else:
+            line = newLine[:]
+    SumLine += len(line)
+    return lenTotal
 
 
 if __name__ == "__main__":
     main()
+    total = 228668
